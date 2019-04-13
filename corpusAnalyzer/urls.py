f8440e9141abdd1d69271e6717c11f15a991ae39
xingzhi <xingzhi2@illinois.edu>
2019-04-13 04:56:01 -0500

    后端完成 请前端工程师加油

--- corpusAnalyzer/urls.py
+++ corpusAnalyzer/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index_page),
-    path("ajax/handle_extract_file_term/<int:queryid>", views.handle_extract_file_term, name="handle_extract_file_term"),
-    # path("ajax/<str:receiver>/all", views.all_msg_ajax),
+    path("ajax/handle_extract_file_term/<int:queryid>", views.handle_extract_file_term,
+         name="handle_extract_file_term"),
+    path("ajax/return_extracted_file/<int:queryid>", views.return_extracted_file, name="return_extracted_file"),
 ]
--- corpusAnalyzer/views.py
+++ corpusAnalyzer/views.py
@@ -4,6 +4,10 @@ from django.http.response import HttpResponse, JsonResponse, FileResponse, Http4
 from django.views.decorators.http import require_http_methods
 import os, json
 from django.core.serializers import serialize
+from tika import parser
+import os.path
+from utils import justeson_extractor
+import re
 
 
 # Create your views here.
@@ -14,3 +18,31 @@ def index_page(request):
 
 def handle_extract_file_term(request, queryid):
     print("hi, I am here")
+    file_to_parse = request.FILES.get("selected_files")
+    to_parse_filename = os.path.join("pdfs", str(queryid) + ".pdf")
+    with open(to_parse_filename, 'wb') as f:
+        f.write(file_to_parse.read())
+
+    raw = parser.from_file(to_parse_filename)
+    plain_text = raw['content']
+
+    reg_exp = '((A|N)+|((A|N)*(NP)?)(A|N)*)N'
+    p = re.compile(reg_exp)
+    doc = str(plain_text).encode('ascii', 'ignore').decode("ascii")
+    min_freq = 2
+
+    terms = justeson_extractor.get_all_terms_in_doc(reg_exp, doc, min_freq)
+
+    result_txt = "\n".join(list(terms))
+    result_path = os.path.join("texts", str(queryid) + ".txt")
+
+    with open(result_path, "w+") as f:
+        f.write(result_txt)
+
+    response = HttpResponse()
+    response.write(f"{result_txt}")
+    return response
+
+
+def return_extracted_file(request, queryid):
+    pass
